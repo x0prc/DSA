22d466e1ef21cbc543b34217983d1028c5ccb7e6
PRC <105520102+x0prc@users.noreply.github.com>
2024-06-23 21:36:25 +0530

    Hashing Algos

---
+++ Hashing/numberHashing.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class numberHashing {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		int[] hash = new int[13];
+		for(int i = 0; i < n; i++) {
+			hash[arr[i]] += 1;
+		}
+		
+		int q;
+		q = sc.nextInt();
+		while (q-- != 0) {
+			int number;
+			number = sc.nextInt();
+			System.out.println(hash[number]);
+		}
+	}
+}
