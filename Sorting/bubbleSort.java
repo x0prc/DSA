096a180102e6c0025b20ddc47d2544d0a7f7e3a9
PRC <105520102+x0prc@users.noreply.github.com>
2024-06-24 21:17:01 +0530

    Sorting Algos

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class bubbleSort{
+	static void bubble_sort(int[] arr, int n) {
+		for (int i = n - 1; i >= 0; i--) {
+			int didSwap = 0;
+			
+			for (int j = 0; j <= i - 1; j++) {
+				if (arr[j] > arr[j + 1]){
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					didSwap = 1;
+				}
+			}
+			if (didSwap == 0) {
+				break;
+			}
+		}
+		System.out.println("After bubble sort");
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + "");
+		}
+		System.out.println();
+	}
+	public static void main(String args[]) {
+		int arr[] = {13, 46, 24, 52, 20, 9};
+		int n = arr.length;
+		System.out.println("Before bubble sort");
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + "");
+		}
+		System.out.println();
+		bubble_sort(arr, n);
+	}
+}
---
+++ Sorting/insertionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+	static void insertion_sort(int[] arr, int n) {
+		for (int i = 0; i <= n - 1; i++) {
+			int j = i;
+			while(j > 0 && arr[j-1] > arr[j]){
+				int temp = arr[j-1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+				j--;
+			}
+		}
+		
+		System.out.println("After insertion sort: ");
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + "");
+		}
+		System.out.println();
+	}
+	public static void main(String args[]) {
+		int arr[] = {13, 46, 24, 5, 20, 9};
+		int n = arr.length;
+		System.out.println("Before insertion sort");
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + "");
+		}
+		System.out.println();
+		insertion_sort(arr, n);
+	}
+}
