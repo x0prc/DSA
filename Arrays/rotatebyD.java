327b89f8a2d56373f20726199ef3aa5f0ec95478
PRC <105520102+x0prc@users.noreply.github.com>
2024-06-25 20:17:09 +0530

    Update rotatebyD.java

--- Arrays/rotatebyD.java
+++ Arrays/rotatebyD.java
@@ -11,4 +11,23 @@ public class rotatebyD{
 			end--;
 		}
 	}
+	
+	public static void RotateToLeft(int[] arr, int n, int k) {
+		Reverse(arr, 0, k - 1); //first k elements
+		Reverse(arr, k, n - 1); //last n-k elements
+		Reverse(arr, 0, n - 1); //reverse whole array
+		
+	}
+	public static void main(String args[]) {
+		int[] arr = {1,2,3,4,5,6,7};
+		int n = 7;
+		int k = 2;
+		
+		RotateToLeft(arr, n, k);
+		System.out.print("After rotating k elements to left");
+		
+		for(int i = 0; i < n; i++)
+			System.out.println(arr[i] + "");
+		System.out.println();
+	}
 }
