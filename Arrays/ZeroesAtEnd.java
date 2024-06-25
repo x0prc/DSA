b9917c07e4b400ed0c8567be53dbbbdcbcba8056
PRC <105520102+x0prc@users.noreply.github.com>
2024-06-25 20:27:59 +0530

    Create ZeroesAtEnd.java

---
+++ Arrays/ZeroesAtEnd.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ZeroesAtEnd{
+	public static int[] moveZeroes(int n, int[]a) {
+		int j = -1;
+		for(int i = 0; i < n; i++){
+			if (a[i] == 0) {
+				j = i;
+				break;
+			}
+		}
+		if (j == -1) return a; // for non-zero elements
+		
+		for(int i = j + 1; i < n; i++) {
+			if (a[i] != 0) {
+				int tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+				j++;
+			}
+		}
+		return a;
+	}
+	public static void main(String[] args){
+		int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+		int n = 10;
+		int[] ans = moveZeroes(n, arr);
+		for(int i = 0; i < n; i++) {
+			System.out.print(ans[i] + "");
+		}
+		System.out.println("");
+		}
+	}
+}
