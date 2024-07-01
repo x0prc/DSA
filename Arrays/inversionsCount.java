a11c589e6603ef69c2912cbd4424ac50a2efe0f8
PRC <105520102+x0prc@users.noreply.github.com>
2024-07-01 11:10:54 +0530

    Create inversionsCount.java

---
+++ Arrays/inversionsCount.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public alass inversionsCount {
+	private static int merge(int[] arr, int low, int mid, int high){
+		ArrayList<Integer> temp = new ArrayList<>();
+		int left = low;
+		int right = mid + 1;
+		int cnt = 0;
+		
+		//elements in sorted temp array
+		while(left <= mid && right <= high) {
+			if(arr[left] <= arr[right]) {
+				temp.add(arr[left]);
+				left++;
+			} else {
+				temp.add(arr[right]);
+				cnt += (mid - left + 1);
+				right++
+			}
+		}
+		// if elements on left are still left
+		while(left <= mid) {
+			temp.add(arr[left]);
+			left++;
+		}
+		// if elements on right are still left
+		while (right <= high) {
+			temp.add(arr[right]);
+			right++;
+		}
+		
+		for(int i = low; i <= high; i++){
+			arr[i] = temp.get(i - low);
+		}
+		return cnt;
+	}
+	
+	pubic static int mergeSort(int[] arr, int low, int high){
+		int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+	}
+		public static int numberOfInversions(int[] a, int n) {
+			return mergeSort(a, 0, n - 1); //number of pairs
+		}
+		
+		public static void main(String[] args){
+			int[] a = {5, 4, 3, 2, 1};
+			int n = 5;
+			int cnt = numberOfInversions(a, n);
+			System.out.println("The number of inversions are" + cnt);
+		}
+
+}	
+
