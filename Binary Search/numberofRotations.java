b155dda3ace77eb62ff45bf91b831d4a07c68a7b
PRC <105520102+x0prc@users.noreply.github.com>
2024-07-03 11:33:28 +0530

    rotations + singleElement

---
+++ Binary Search/numberofRotations.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class numberofRotations {
+    public static int findKRotation(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+             
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                // Eliminate left half:
+                low = mid + 1;
+            } else { 
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
