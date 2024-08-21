0f580742ebcf9cdfa8a0b99036601be2fa005458
PRC <105520102+nocaprc@users.noreply.github.com>
2024-08-21 20:21:08 +0530

    okie dokie

---
+++ operations.c
@@ -0,0 +1,19 @@
+// traversal
+#include <stdio.h>
+#include <iostream>
+
+traverse(struct node* head)
+{
+  if(head == null)
+    printf("linked list empty");
+
+  struct node *ptr;
+  ptr = head;
+
+  while(ptr != null)
+  {
+    printf("%d", ptr -> data);
+    ptr = ptr -> next; 
+  }
+}
+
