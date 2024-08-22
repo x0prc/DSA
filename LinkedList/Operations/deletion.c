3ecfe4b6d85c8428943f746d09508221c34a1fb5
PRC <105520102+x0prc@users.noreply.github.com>
2024-08-22 20:05:12 +0530

    sdasdadsdkbg

--- LinkedList/Operations/deletion.c
+++ LinkedList/Operations/deletion.c
@@ -7,4 +7,62 @@ struct node {
 }
 
 // deletion from beginning
+delfirst(struct node *head)
+{
+  struct node *ptr;
+  if(head == null){
+    printf("list is already empty");
+  }
+  else {
+    ptr = head;
+    head = head -> next;
+    free(ptr);
+  }
+  return head;
+}
 
+//deletion from end
+del_last(struct node *head){
+  struct node *ptr, *prep
+  if(head == null)
+    printf("list is empty");
+  
+  elseif(head -> next == null)
+  {
+    free(head);
+    head = null;
+  }
+
+  else {
+    ptr = head;
+    while(ptr -> next != null){
+      prep = ptr;
+      ptr = ptr -> next;
+    }
+    prep -> next = null;
+    free(ptr);
+  }
+
+  return head;
+
+}
+
+//deletion after a node
+delete_after(struct node *head, int key){
+  struct node = *ptr1, *ptr2;
+  ptr1 = head;
+  
+while(ptr1 -> next != null){
+  if(ptr1 -> data == key)
+  {
+    ptr2 = ptr1 -> next;
+    ptr1 -> next = ptr2 -> next;
+    free(ptr2);
+    break;
+  }
+  ptr1 = ptr1 -> next;
+}
+
+return head;
+
+}
